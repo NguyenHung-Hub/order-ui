b5a2a64c9a2a7f672fe18c44e866eacc739c0e4c
Hung <daminhnguyenhung@gmail.com>
2023-07-18 00:07:25 +0700

    fix env to deploy

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,13 @@ const config = {
     BASE_URL: import.meta.env.VITE_BASE_URL,
     DOMAIN: import.meta.env.VITE_API_DOMAIN,
     PORT: import.meta.env.VITE_API_PORT,
+    MODE: import.meta.env.MODE,
+    DEV: import.meta.env.DEV,
+    PROD: import.meta.env.PROD,
+    SSR: import.meta.env.SSR,
+    SOCKET_URL: import.meta.env.VITE_SOCKET_URL,
 };
 
-console.log(process.env.NODE_ENV);
+console.log(config.PROD);
 
 export default config;
